c3f7c1487e9c3c01c7aafb267028383c766b2453
Yuriy Chemerys <yuriy.chemerys@indigitus.ch>
2014-03-06 14:08:55 +0200

    Add Routing.

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,5 +1,23 @@
+import routes
 import webob
 import webob.dec
+import webob.exc
+
+route_map = routes.Mapper()
+controller = 'number'
+route_map.connect('1', '/1', controller=controller, action='one',
+                  conditions={'method':['GET']})
+route_map.connect('2', '/2', controller=controller, action='two',
+                  conditions={'method':['GET']})
+
+
+class Number(object):
+    def one(self, request):
+        return webob.Response(body='One!')
+
+    def two(self, request):
+        return webob.Response(body='Two!')
+
 
 class TestApp(object):
     def __init__(self):
@@ -7,7 +25,16 @@ class TestApp(object):
 
     @webob.dec.wsgify
     def __call__(self, request):
-        return webob.Response(body='Hello my friend!')
+        match = route_map.match(environ=request.environ,
+                                url=request.path_info)
+        if match:
+            controller_name = match['controller']
+            action_name = match['action']
+            if controller_name == 'number':
+                controller = Number()
+                return getattr(controller, action_name)(request)
+
+        return webob.exc.HTTPNotFound()
 
 
 class TestFilter(object):
